24995e8bf39b8641764669c02d9d3ff1993c111a
KevnAp <apariciodkevin@Gmail.com>
2026-04-13 11:44:58 -0500

    uploaded jacovs node file and html

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,12 @@
 // Where the doubly linked list uses a song object and points to the next song
 // and the previous song
+
+public class Node {
+    int key;
+    Node next, prev;
+
+    public Node(int key) {
+        this.key = key;
+        this.prev = this.next = null;
+    }
+}
