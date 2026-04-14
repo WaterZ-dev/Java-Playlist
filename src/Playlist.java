893d413c50f537c2b2fbf9cdd9b79eaad116a46f
WaterZ-dev <74316139+WaterZ-dev@users.noreply.github.com>
2026-04-14 08:22:05 -0500

    Update Playlist.java
    File/Audio Loader added

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,135 +1,22 @@
-// Playlist will be similar to doubly linked list file.
-// Playlist needs to play/pause, skip, back, add song,
-// remove song, display song. and if we have time shuffle.
+//Playlist functions as the main file and works with DoublyLinkedList to load and navigate through the audio files
 
+public class Playlist {
 
-
-
-// Doubly linked list for reference.
-/*public class DoublyLinkedList {
-    int size = 0;
-    Node head, tail;
-
-    public DoublyLinkedList() {
-        this.head = this.tail = null;
-    }
-
-    public void addFirst(int num) {
-        Node node = new Node(num);
-        if (head == null) {
-            head = tail = node;
-        } else {
-            node.next = head;
-            head.prev = node;
-            head = node;
-        }
-        size++;
-    }
-
-    public void addLast(int num) {
-        Node node = new Node(num);
-        if (tail == null) {
-            head = tail = node;
-        } else {
-            tail.next = node;
-            node.prev = tail;
-            tail = node;
-        }
-        size++;
-    }
-
-    public void add(int index, int num) {
-        Node temp = new Node(num);
-        if (index == 1) {
-            addFirst(num);
-        } else {
-            Node node = head;
-            for (int i = 1; i < index; i++) {
-                if (node != null)
-                    node = node.next;
-            }
-            if (node == null) {
-                addLast(num);
-            } else {
-                temp.next = node;
-                temp.prev = node.prev;
-                node.prev.next = temp;
-                node.prev = temp;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DoublyLinkedList<Song> playlist = new DoublyLinkedList<>();
+        Node<Song> currentSong;
+        //load songs
+        File folder = new File("resources/audio");
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            if (file.getName().endsWith(".mp3")) {
+            	playlist.addLast(new Song(
+                    file.getName(),
+                    file.getPath()
+                ));
             }
         }
-        size++;
+        currentSong = playlist.get(0);
     }
-
-    public void deleteFirst() {
-        if (head == null)
-            return;
-        if (head == tail) {
-            head = tail = null;
-            return;
-        }
-        Node node = head;
-        head = head.next;
-        head.prev = node.next = null;
-        size--;
-    }
-
-    public void deleteLast() {
-        if (tail == null)
-            return;
-        if (tail == head) {
-            tail = head = null;
-            return;
-        }
-        Node node = tail;
-        tail = tail.prev;
-        tail.next = node.prev = null;
-        size--;
-    }
-
-    public void delete(int index) {
-        Node node = head;
-        if (index == 1) {
-            deleteFirst();
-            return;
-        }
-        for (int i = 1; i <= index; i++) {
-            if (node != null)
-                node = node.next;
-        }
-        if (node == null)
-            return;
-        if (node == tail) {
-            deleteLast();
-            return;
-        }
-    }
-
-    public String toString() {
-        String str = "";
-        Node node = head;
-        while (node != null) {
-            str += node.key;
-            if (node.next != null) {
-                str += " | ";
-            }
-            node = node.next;
-        }
-        return str;
-    }
-
-    public int size() {
-        int i = 0;
-        for (Node node = head; node != null; node = node.next)
-            i++;
-        return i;
-
-    }
-
-    public int get(int index) {
-        Node node = head;
-        for (int i = 0; i < index; i++) {
-            node = node.next;
-        }
-        return node.key;
-    }
-}*/
+}
