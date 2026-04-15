d06c765cf3765194b9de5e2d81240f2148197950
KevnAp <apariciodkevin@Gmail.com>
2026-04-14 22:16:12 -0500

    testing main file

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-
-static void main(String[] args) {
+class Main {
     public static void main(String[] args) {
         Playlist playlist = new Playlist();
 
-        // Test addSong
         playlist.addSong("Song 1", "Artist 1", "resources/audio/song1.mp3");
         playlist.addSong("Song 2", "Artist 2", "resources/audio/song2.mp3");
         playlist.addSong("Song 3", "Artist 3", "resources/audio/song3.mp3");
 
-        System.out.println("Size after adding: " + playlist.size);
+        playlist.removeSong("Song 2");
+    }
 }
