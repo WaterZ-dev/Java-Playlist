a2fe55cb1cbef6fcb4f7f7a62daa275d10a850a8
KevnAp <apariciodkevin@Gmail.com>
2026-04-14 21:18:34 -0500

    EDdited playlist.java and opened up my own branch

--- src/Playlist.java
+++ src/Playlist.java
@@ -2,21 +2,71 @@
 
 public class Playlist {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        DoublyLinkedList<Song> playlist = new DoublyLinkedList<>();
-        Node<Song> currentSong;
-        //load songs
-        File folder = new File("resources/audio");
-        File[] files = folder.listFiles();
-        for (File file : files) {
-            if (file.getName().endsWith(".mp3")) {
-            	playlist.addLast(new Song(
-                    file.getName(),
-                    file.getPath()
-                ));
+    private Song head = null;
+    private Song tail = null;
+    private int size = 0;
+
+    public Playlist(){ // Constructor
+
+    }
+
+    // PLAYLIST METHODS
+
+    public void addSong(String title, String artist, String filepath){
+        Song track = new Song(title, artist, filepath);
+        if (size == 0){
+            head = track;
+            tail = track;
+        } else {
+            track.prev = tail;
+            tail.next = track;
+            tail = track;
+        }
+        size++;
+    }
+
+    public void removeSong(String title){
+        Song songName = head;
+        if (size == 0){ // If playlist is EMPTY
+            System.out.println("Playlist is EMPTY");
+        } else if (size == 1){ // If there is only 1 song in the playlist
+            head = null;
+            tail = null;
+            size--;
+        } else {
+            while (songName != null){
+
+                if(songName.getTitle().equals(title)){
+
+                    if (songName == head) { // If the song to delete is the first one(head)
+                        head = songName.getNext();
+
+                        if (head != null){ // So code doesn't break (null exception)
+                            head.setPrev(null);
+                        }
+                        size--;
+                        break;
+                    }
+
+                    else if (songName == tail){ // if the song to delete is the last one(tail)
+                        tail = songName.getPrev();
+
+                        if (tail != null){
+                            tail.setNext(null);
+                        }
+                        size--;
+                        break;
+                    } else { // Deletes song by name and if it doesn't satisfy the other cases.
+                        songName.getPrev().setNext(songName.getNext());
+                        songName.getNext().setPrev(songName.getPrev());
+                        size--;
+                        break;
+                    }
+                }
+                songName = songName.getNext();
             }
         }
-        currentSong = playlist.get(0);
+
+
     }
 }
