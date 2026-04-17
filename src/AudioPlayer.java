47403dd3fd35cc27b3a10a944aa4a18f99c7178f
WaterZ-dev <74316139+WaterZ-dev@users.noreply.github.com>
2026-04-17 11:07:23 -0500

    Update AudioPlayer.java

--- src/AudioPlayer.java
+++ src/AudioPlayer.java
@@ -3,17 +3,17 @@ import java.io.FileInputStream;
 
 public class AudioPlayer {
 
-    public void play(String path) {
-    new Thread(() -> {
-        try {
-            FileInputStream fis = new FileInputStream(path);
-            Player player = new Player(fis);
-            player.play();
-        } catch (Exception e) {
-            System.out.println("Playback failed.");
-        }
-    }).start();
+	public void play(String path) {
+		new Thread(() -> {
+			try {
+				FileInputStream fis = new FileInputStream(path);
+				Player player = new Player(fis);
+				player.play();
+			} catch (Exception e) {
+				System.out.println("Playback failed.");
+			}
+		}).start();
 
-    System.out.println("Playing: " + path);
-}
+		System.out.println("Playing: " + path);
+	}
 }
